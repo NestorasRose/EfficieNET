0e6341ab96853578ad019a3f2712c44aa38d3125
Your Name <you@example.com>
2021-04-25 01:34:40 +0300

    Add project files.

---
+++ EfficieNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EfficieNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
